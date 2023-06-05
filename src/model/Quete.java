b8a930e66f9f6c3e1693cba3d8e2d41a30bdd2b6
mooi03 <timothee.lonchampt@gmail.com>
2023-06-05 14:23:23 +0200

    ajoute de la classe quête avec les methodes : constructeur, toString et Get pas de teste car methode trops simple

--- src/model/Quete.java
+++ src/model/Quete.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.util.Scanner;
+
 public class Quete {
+    private int numero ;
+    private Position pos ;
+    private Precond precond ;
+    private int duree ;
+    private int experience ;
+    private String intitule ;
+    public Quete(String ligne) {
+
+        Scanner scanner = new Scanner(ligne).useDelimiter("\\|");
+
+        while (scanner.hasNext()) {
+
+            numero = scanner.nextInt();
+
+            pos = new Position( scanner.next() ) ; /// on crée un objet de la classe Position
+
+            precond = new Precond( scanner.next() );
+
+            duree = scanner.nextInt();
+
+            experience = scanner.nextInt();
+
+            intitule = scanner.next();
+        }
+
+    }
+    public String toString() {
+        return numero + "-" + pos + "-" + precond + "-" + duree + "-" + experience + "-" + intitule ;
+    }
+    public int getNumero() {
+        return numero ;
+    }
+    public Precond getPrecond() {
+        return precond ;
+    }
+
 }
