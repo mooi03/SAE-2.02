a13dabc6f5cb611697b0df64153be30cbe41736c
mooi03 <timothee.lonchampt@gmail.com>
2023-06-07 18:21:09 +0200

    ajout du scan des scenario pour pouvoir instencier les quête

--- src/model/Scan.java
+++ src/model/Scan.java
@@ -1,4 +1,30 @@
-package model;
+package model ;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scan {
+    public static List<Quete> Lecture (File fichier) {
+        List<Quete> quetes = new ArrayList<>() ;
+
+        try {
+
+            java.util.Scanner scanner = new java.util.Scanner(fichier);
+
+            while (scanner.hasNext ()) {
+
+                Quete quete = new Quete(scanner.nextLine());
+                quetes.add(quete) ;
+            }
+        }
+
+        catch (FileNotFoundException e) {
+
+            System.err.println(e.getMessage());
+
+        }
+        return quetes;
+
+    }
 }
