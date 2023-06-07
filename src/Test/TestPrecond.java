d3a02404da23a1e093ce009867de0e96741ddbd4
mooi03 <timothee.lonchampt@gmail.com>
2023-06-07 18:17:29 +0200

    ajout de la methode estPossible et de son teste et  des doc string dans précond

--- src/Test/TestPrecond.java
+++ src/Test/TestPrecond.java
@@ -1,7 +1,12 @@
 package Test;
-
 import model.Precond;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 public class TestPrecond {
     public static void main(String[] args) {
         String[] strTest = {"()", "((2,3),(1,))", "((4,),(5,))", "((2,3),)", "((2,),)", "((8,),(7,4))"};
@@ -17,5 +22,94 @@ public class TestPrecond {
                 System.out.println("le cas " + i + " marche bien");
             }
         }
+    }
+    @Test
+    public void TestEstPossible() {
+        int w = 14;
+        int x = 12;
+        int y = 21;
+        int z = 17;
+        Precond precond1 = new Precond("(0,0,0,0)");
+        Precond precond2 = new Precond("(0,0,"+y+",0)");
+        Precond precond3 = new Precond("("+w+",0,0,0)");
+        Precond precond4 = new Precond("(0,0,"+y+","+z+")");
+        Precond precond5 = new Precond("("+w+",0,"+y+",0)");
+        Precond precond6 = new Precond("("+w+","+x+",0,0)");
+        Precond precond7 = new Precond("("+w+",0,"+y+","+z+")");
+        Precond precond8 = new Precond("("+w+","+x+","+y+",0)");
+        Precond precond9 = new Precond("("+w+","+x+","+y+","+z+")");
+
+        List<Integer> ListeVide = new ArrayList<>();
+        List<Integer> listZ = new ArrayList<>(Arrays.asList(z));
+        List<Integer> listY = new ArrayList<>(Arrays.asList(y));
+        List<Integer> listYZ = new ArrayList<>(Arrays.asList(y, z));
+        List<Integer> listX = new ArrayList<>(Arrays.asList(x));
+        List<Integer> listXZ = new ArrayList<>(Arrays.asList(x, z));
+        List<Integer> listXY = new ArrayList<>(Arrays.asList(x, y));
+        List<Integer> listXYZ = new ArrayList<>(Arrays.asList(x, y, z));
+        List<Integer> listW = new ArrayList<>(Arrays.asList(w));
+        List<Integer> listWZ = new ArrayList<>(Arrays.asList(w, z));
+        List<Integer> listWY = new ArrayList<>(Arrays.asList(w, y));
+        List<Integer> listWYZ = new ArrayList<>(Arrays.asList(w, y, z));
+        List<Integer> listWX = new ArrayList<>(Arrays.asList(w, x));
+        List<Integer> listWXZ = new ArrayList<>(Arrays.asList(w, x, z));
+        List<Integer> listWXY = new ArrayList<>(Arrays.asList(w, x, y));
+        List<Integer> listWXYZ = new ArrayList<>(Arrays.asList(w, x, y, z));
+
+        //Test Classe1
+        assertTrue(precond1.estPossible(ListeVide)); //vide
+
+        //Test Classe2 Y
+        assertFalse(precond2.estPossible(ListeVide)); //vide
+        assertTrue(precond2.estPossible(listY)); //1 condition
+
+        //Test Classe3 W
+        assertFalse(precond3.estPossible(ListeVide)); //vide
+        assertTrue(precond3.estPossible(listW)); //1 condition
+
+        // Test Classe4 YZ
+        assertFalse(precond4.estPossible(ListeVide)); //vide
+        assertTrue(precond4.estPossible(listY)); //1 condition
+        assertTrue(precond4.estPossible(listYZ)); //2 conditions
+
+        // Test Classe5 WY
+        assertFalse(precond5.estPossible(ListeVide)); //vide
+        assertFalse(precond5.estPossible(listW)); //1 condition
+        assertFalse(precond5.estPossible(listY)); //1 condition
+        assertTrue(precond5.estPossible(listWY)); //2 conditions
+
+        // Test Classe6 WX
+        assertFalse(precond6.estPossible(ListeVide)); //vide
+        assertTrue(precond6.estPossible(listW)); //1 condition
+        assertTrue(precond6.estPossible(listX)); //1 condition
+        assertTrue(precond6.estPossible(listWX)); //2 condition
+
+        // Test Classe7 WYZ
+        assertFalse(precond7.estPossible(ListeVide)); //vide
+        assertFalse(precond7.estPossible(listW)); //1 condition
+        assertFalse(precond7.estPossible(listY)); //1 condition
+        assertFalse(precond7.estPossible(listZ)); //1 condition
+        assertFalse(precond7.estPossible(listYZ)); //2 condition dans le même duo
+        assertTrue(precond7.estPossible(listWY)); //2 condition dans des duo differents
+        assertTrue(precond7.estPossible(listWZ)); //2 condition dans des duo differents
+        assertTrue(precond7.estPossible(listWYZ)); //3 conditions
+
+        // Test Classe9 WXYZ
+        assertFalse(precond9.estPossible(ListeVide)); //vide
+        assertFalse(precond9.estPossible(listW)); //1 condition
+        assertFalse(precond9.estPossible(listX)); //1 condition
+        assertFalse(precond9.estPossible(listY)); //1 condition
+        assertFalse(precond9.estPossible(listZ)); //1 condition
+        assertFalse(precond9.estPossible(listYZ)); //2 condition dans le même duo
+        assertFalse(precond9.estPossible(listWX)); //2 condition dans le même duo
+        assertTrue(precond9.estPossible(listWY)); //2 condition dans des duo differents
+        assertTrue(precond9.estPossible(listWZ)); //2 condition dans des duo differents
+        assertTrue(precond9.estPossible(listXZ)); //2 condition dans des duo differents
+        assertTrue(precond9.estPossible(listXY)); //2 condition dans des duo differents
+        assertTrue(precond9.estPossible(listWYZ)); //3 conditions
+        assertTrue(precond9.estPossible(listWXY)); //3 conditions
+        assertTrue(precond9.estPossible(listWXZ)); //3 conditions
+        assertTrue(precond9.estPossible(listXYZ)); //3 conditions
+        assertTrue(precond9.estPossible(listWXYZ)); //4 conditions
     }
 }
--- src/model/Precond.java
+++ src/model/Precond.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.List;
 import java.util.Scanner;
 
 public class Precond {
@@ -32,6 +33,43 @@ public class Precond {
         condition1 = tabprecond [1] ;
         condition2 = tabprecond [2] ;
         condition3 = tabprecond [3] ;
+    }
+
+    /**
+     * Vérifie si les préconditions des quêtes sont remplies en fonction des quêtes déjà terminées.
+     *
+     * @param listFini liste des quêtes déjà finis
+     * @return true si les deux duo de préconditions des quêtes ont au moins une quête réalisée, sinon false
+     */
+    public boolean estPossible(List<Integer> listFini) {
+        int[] duoCond1 = {condition0,condition1} ;
+        int[] duoCond2 = {condition2,condition3} ;
+        int add1 = condition0 + condition1 ;
+        int add2 = condition2 + condition3 ;
+        // Pour traiter le cas de la toute première quête
+        if (add1 == 0 && add2 == 0){
+            return true ;
+        }
+        // on met a jour la valeur des précondition pour des quêtes déjà faites
+        for (Integer e : listFini) {
+            for (int i = 0 ; i < 2 ; i++){
+                if (duoCond1[i] == e){
+                    duoCond1[i] = 0 ;
+                }
+                if (duoCond2[i] == e){
+                    duoCond2[i] = 0 ;
+                }
+            }
+        }
+        int nvDuo1 = duoCond1[0]+duoCond1[1] ;
+        int nvDuo2 = duoCond2[0]+duoCond2[1] ;
+        // Vérifie si les valeurs des duo de précondition a changé (ce qui voudrait dire qu'une des 2 quête est réalisé)
+        if ((add1 > nvDuo1 || nvDuo1 == 0) && (add2 > nvDuo2 || nvDuo2 == 0)){
+            return true ;
+        }
+        else {
+            return false ;
+        }
     }
 
     public String toString() {
