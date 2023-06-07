68f140ed0bca0e2d20639ef5bcce82893c2338a8
mooi03 <timothee.lonchampt@gmail.com>
2023-06-07 18:34:41 +0200

    ajout de la methode graphe constructeur et toString

--- src/model/Graphe.java
+++ src/model/Graphe.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.List;
+import java.util.TreeMap;
+
 public class Graphe {
+    private TreeMap<Integer,Precond> dicoGraphe = new TreeMap<>();
+    public Graphe(List<Quete> quetes){
+        for ( Quete point : quetes ){
+            dicoGraphe.put(point.getNumero(), point.getPrecond()) ;
+        }
+    }
+    public String toString() {
+        return "Graphe{" +
+                "dicoGraphe=" + dicoGraphe +
+                '}';
+    }
 }
+
