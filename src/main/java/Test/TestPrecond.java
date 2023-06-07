96768de2b7ae712a4153df54c65083c101ed7656
mooi03 <timothee.lonchampt@gmail.com>
2023-06-08 01:26:36 +0200

    modification de test precond car incomplet

--- src/main/java/Test/TestPrecond.java
+++ src/main/java/Test/TestPrecond.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public class TestPrecond {
     public static void main(String[] args) {
-        String[] strTest = {"()", "((2,3),(1,))", "((4,),(5,))", "((2,3),)", "((2,),)", "((8,),(7,4))"};
-        Precond[] presondTest = {new Precond(0, 0, 0, 0), new Precond(2, 3, 1, 0), new Precond(4, 0, 5, 0), new Precond(2, 3, 0, 0), new Precond(2, 0, 0, 0), new Precond(8, 0, 7, 4)};
-        for (int i = 0 ; i < 6 ; i++ ){
+        String[] strTest = {"()", "((2,3),(1,))", "((4,),(5,))", "((2,3),)", "((2,),)", "((8,),(7,4))", "((8,9),(7,4))"};
+        Precond[] presondTest = {new Precond(0, 0, 0, 0), new Precond(2, 3, 1, 0), new Precond(4, 0, 5, 0), new Precond(2, 3, 0, 0), new Precond(2, 0, 0, 0), new Precond(8, 0, 7, 4), new Precond(8,9,7,4)};
+        for (int i = 0 ; i < 7 ; i++ ){
             Precond compPrecond = new Precond(strTest[i]) ;
             System.out.println(compPrecond);
             System.out.println(presondTest[i]);
