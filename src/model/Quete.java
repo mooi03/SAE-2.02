43cdb3ae327248dfebd3bba3d228a2ed2d70e341
mooi03 <timothee.lonchampt@gmail.com>
2023-06-07 23:57:23 +0200

    ajout des donc string de Quete et Scan

--- src/model/Quete.java
+++ src/model/Quete.java
@@ -1,7 +1,10 @@
 package model;
 
 import java.util.Scanner;
-
+/**
+ * La classe Quete représente une quête avec ses attributs tels que le numéro, la position, les préconditions,
+ * la durée, l'expérience et l'intitulé.
+ */
 public class Quete {
     private int numero ;
     private Position pos ;
@@ -9,6 +12,11 @@ public class Quete {
     private int duree ;
     private int experience ;
     private String intitule ;
+    /**
+     * Constructeur de la classe Quete qui initialise les attributs à partir d'une ligne donnée.
+     *
+     * @param ligne La ligne contenant les informations de la quête séparées par des '|'.
+     */
     public Quete(String ligne) {
 
         Scanner scanner = new Scanner(ligne).useDelimiter("\\|");
@@ -29,16 +37,46 @@ public class Quete {
         }
 
     }
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de la quête.
+     *
+     * @return Une chaîne de caractères représentant la quête.
+     */
     public String toString() {
         return numero + "-" + pos + "-" + precond + "-" + duree + "-" + experience + "-" + intitule ;
     }
+    /**
+     * Retourne le numéro de la quête.
+     *
+     * @return Le numéro de la quête.
+     */
     public int getNumero() {
         return numero ;
     }
+    /**
+     * Retourne les préconditions de la quête.
+     *
+     * @return Les préconditions de la quête.
+     */
     public Precond getPrecond() {
         return precond ;
     }
+    /**
+     * Retourne la position de la quête.
+     *
+     * @return La position de la quête.
+     */
     public Position getPos(){return pos ;}
+    /**
+     * Retourne l'expérience de la quête.
+     *
+     * @return L'expérience de la quête.
+     */
     public int getExperience(){return experience ;}
+    /**
+     * Retourne la durée de la quête.
+     *
+     * @return La durée de la quête.
+     */
     public int getDuree(){ return duree ;}
 }
--- src/model/Scan.java
+++ src/model/Scan.java
@@ -3,8 +3,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * La classe Scan fournit des méthodes pour la lecture de fichiers contenant des quêtes.
+ */
 public class Scan {
+    /**
+     * Lit le contenu d'un fichier et retourne une liste de quêtes.
+     *
+     * @param fichier Le fichier à lire.
+     * @return Une liste de quêtes.
+     */
     public static List<Quete> Lecture (File fichier) {
         List<Quete> quetes = new ArrayList<>() ;
 
