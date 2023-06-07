15dfa1c20771657f4470263e1f144d4ce3b9ea0f
mooi03 <timothee.lonchampt@gmail.com>
2023-06-07 23:45:00 +0200

    finalisation pour que tout ce lance

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import model.Algo;
 import model.Precond;
 
 public class Main {
     public static void main(String[] args) {
-
+        Algo nes = new Algo() ;
+        nes.GlouExhaustive("scenario_10.txt");
+        nes.GlouEfficace("scenario_10.txt");
     }
 }
